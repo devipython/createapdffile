ecd65776a87445e8c39e8f8a934c60df39c1decb
devi <devi.jeevan@gmail.com>
2024-09-23 13:05:57 +0530

    added comments with initial Commit

--- projcode.py
+++ projcode.py
@@ -9,7 +9,7 @@ for index,row in df.iterrows():
     pdf.add_page()
     #Cell1 set the font for each cell
     pdf.set_font( family="Times",size=24,style="B")
-    pdf.set_text_color(100,100,100)
+    pdf.set_text_color(100,100,100) #whitecolor is 250:250:250
     pdf.cell(w=0,h=12,txt = row["Topic"],align="L",ln=1)
     pdf.line(10,21,200,21)
 
