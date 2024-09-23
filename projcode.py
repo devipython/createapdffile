ad05eea00ae00ca25258da84538b8a06def4daa0
devi <devi.jeevan@gmail.com>
2024-09-23 13:44:14 +0530

    added footer no of pages

--- projcode.py
+++ projcode.py
@@ -2,20 +2,30 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf =FPDF(orientation="P",unit="mm",format="A4")
+pdf.set_auto_page_break(auto=False,margin=0)
+#getting the topics from file
 df= pd.read_csv("topics.csv")
 no_of_pages=0
 
 for index,row in df.iterrows():
-    no_of_pages= no_of_pages+row["Pages"]
+
     for i in range(row["Pages"]):
+
+        no_of_pages = no_of_pages + 1
+
         #adding pages
-        #sum=sum+1
         pdf.add_page()
         #Cell1 set the font for each cell
         pdf.set_font( family="Times",size=24,style="B")
         pdf.set_text_color(100,100,100) #whitecolor is 250:250:250
         pdf.cell(w=0,h=12,txt = row["Topic"],align="L",ln=1)
         pdf.line(10,21,200,21)
+
+        #set the footer with page numbers
+        pdf.ln(260)
+        pdf.set_font(family="Times", size=10, style="I")
+        pdf.set_text_color(180, 180, 180)  # whitecolor is 250:250:250
+        pdf.cell(w=0, h=12, txt=f"Pages {no_of_pages}", align="R")
 
 pdf.output(("Output.pdf"))
 print(f"{no_of_pages} Pages created Successfully")
